98baebd765ca705d6deb5d5d4471eb33bfa79556
zhansaya077 <zhansaya.bekbulat@narxoz.kz>
2026-03-20 20:26:09 +0500

    Fix PoisonCoatingDecorator methods to use wrapped action

--- src/com/narxoz/rpg/decorator/PoisonCoatingDecorator.java
+++ src/com/narxoz/rpg/decorator/PoisonCoatingDecorator.java
@@ -1,22 +1,26 @@
 package com.narxoz.rpg.decorator;
 
 public class PoisonCoatingDecorator extends ActionDecorator {
+
     public PoisonCoatingDecorator(AttackAction wrappedAction) {
         super(wrappedAction);
     }
 
     @Override
     public String getActionName() {
-        return " super.getActionName();
+    
+        return "Venomous " + getWrappedAction().getActionName();
     }
 
     @Override
     public int getDamage() {
-        return super.getDamage() + 3;
+        
+        return getWrappedAction().getDamage() + 3;
     }
 
     @Override
     public String getEffectSummary() {
-        return super.getEffectSummary() ;
+       
+        return getWrappedAction().getEffectSummary() + ", Poison Over Time";
     }
 }
