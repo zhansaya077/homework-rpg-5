e611aa398d3e3656c96be43f3c03e84ac731fa9e
zhansaya077 <zhansaya.bekbulat@narxoz.kz>
2026-03-20 20:21:30 +0500

    Refactor FireRuneDecorator to use getWrappedAction

--- src/com/narxoz/rpg/decorator/FireRuneDecorator.java
+++ src/com/narxoz/rpg/decorator/FireRuneDecorator.java
@@ -1,22 +1,25 @@
 package com.narxoz.rpg.decorator;
 
 public class FireRuneDecorator extends ActionDecorator {
+
     public FireRuneDecorator(AttackAction wrappedAction) {
         super(wrappedAction);
     }
 
     @Override
     public String getActionName() {
-        return "Flaming " + super.getActionName();
+
+        return "Flaming " + getWrappedAction().getActionName();
     }
 
     @Override
     public int getDamage() {
-        return super.getDamage() + 5;
+        return getWrappedAction().getDamage() + 5;
     }
 
     @Override
     public String getEffectSummary() {
-        return super.getEffectSummary() + ", Burn Effect";
+
+        return getWrappedAction().getEffectSummary() + ", Fire Burn";
     }
 }
