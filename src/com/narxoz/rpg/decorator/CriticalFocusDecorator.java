ab6e8bd67b424cb0f8b53f49a2654d5967396718
zhansaya077 <zhansaya.bekbulat@narxoz.kz>
2026-03-20 20:32:09 +0500

    Enhance CriticalFocusDecorator with critical damage logic
    Implemented critical damage and effect behavior in the CriticalFocusDecorator.

--- src/com/narxoz/rpg/decorator/CriticalFocusDecorator.java
+++ src/com/narxoz/rpg/decorator/CriticalFocusDecorator.java
@@ -1,25 +1,26 @@
+
 package com.narxoz.rpg.decorator;
 
 public class CriticalFocusDecorator extends ActionDecorator {
+
     public CriticalFocusDecorator(AttackAction wrappedAction) {
         super(wrappedAction);
     }
 
     @Override
     public String getActionName() {
-        // TODO: Decide how this decorator changes the visible action name.
-        return super.getActionName();
+       
+        return "Crushing " + getWrappedAction().getActionName();
     }
 
     @Override
     public int getDamage() {
-        // TODO: Add critical-related behavior on top of wrapped damage.
-        return super.getDamage();
+        
+        return getWrappedAction().getDamage() * 2;
     }
 
     @Override
     public String getEffectSummary() {
-        // TODO: Append or compose the critical effect description.
-        return super.getEffectSummary();
+        return getWrappedAction().getEffectSummary() + ", Critical Strike Chance";
     }
 }
